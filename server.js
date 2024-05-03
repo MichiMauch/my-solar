a5dd6c63e0fce680fdd69ce75d64ba3c91a4aad9
MichiMauch <michi.mauch@gmail.com>
2024-05-03 09:00:53 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public')); // für statische Dateien wie HTML, CSS und JS
+
+app.get('/solar-status', async (req, res) => {
+  let config = {
+    method: 'get',
+    url: 'https://vrmapi.victronenergy.com/v2/installations/193415/stats?interval=15mins',
+    headers: { 
+        'x-authorization': `Bearer ${process.env.BEARER_TOKEN}`
+    }
+  };
+
+  try {
+    const response = await axios.request(config);
+    const data = response.data.records;
+    const currentPower = data.Pdc[data.Pdc.length - 1][1];
+    const batteryCharge = data.bs[data.bs.length - 1][1]; // Annahme, dass dies das richtige Feld ist
+    res.json({ currentPower: currentPower, batteryCharge: batteryCharge });
+  } catch (error) {
+    console.error('Fehler beim Abrufen der Daten:', error);
+    res.status(500).send('Serverfehler');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server läuft auf http://localhost:${port}`);
+});
